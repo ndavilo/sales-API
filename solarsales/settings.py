f3480679d1e75dc6f9a3f2f2d45d85b6c6a5c9ba
Chukwunonso David Ilonze <nonsodavilo@gmail.com>
2023-07-14 19:28:40 +0100

    modified my settings.py

--- solarsales/settings.py
+++ solarsales/settings.py
@@ -197,7 +197,7 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 # Configuring S3 as the storage backend
-AWS_STORAGE_BUCKET_NAME = 'solar-sales-s3-bucket'
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'us-east-2'  # e.g., 'us-east-1' or 'eu-west-2'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
