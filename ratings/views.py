a29de0f266c1af5707cea06474b67e9120c3588d
Chukwunonso David Ilonze <nonsodavilo@gmail.com>
2023-07-29 22:57:33 +0100

    worked on rating filter by product

--- ratings/views.py
+++ ratings/views.py
@@ -5,18 +5,25 @@ from .models import Rating
 from .serializers import RatingSerializer
 from rest_framework.permissions import IsAdminUser
 
-class RatingCreateView(generics.CreateAPIView):
-    queryset = Rating.objects.all()
-    serializer_class = RatingSerializer
-
+class ProductFilter(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        product_id = request.query_params.get('product')
+        if product_id:
+            return queryset.filter(product_id=product_id)
+        return queryset
+    
 class RatingListView(generics.ListAPIView):
     serializer_class = RatingSerializer
     permission_classes = [AllowAny]
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['product']
+    filter_backends = [ProductFilter, filters.OrderingFilter]
+    filterset_fields = ['product_id']
 
     def get_queryset(self):
         return Rating.objects.all()
+
+class RatingCreateView(generics.CreateAPIView):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
 
 class RatingDeleteView(generics.DestroyAPIView):
     queryset = Rating.objects.all()
